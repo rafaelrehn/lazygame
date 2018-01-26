6422f2e8bf10b729f57f2778ccb2c216ba1bff1f
Rafael Rehn <rafaelluizrehn@gmail.com>
2018-01-26 15:33:27 -0200

    .

--- adduser.php
+++ adduser.php
@@ -1,16 +1,14 @@
 <?php
 	require 'connection.php';
 
-
-
 $nome = $_POST['nome'];
-$tempo = $_POST['tempo'];
-$temporeal = $_POST['temporeal'];
+$tempo = $_POST['tempophp'];
+$temporeal = $_POST['tempophpreal'];
 //echo "$tempo";
 $result_recado="INSERT INTO usuario (nome, tempo, temporeal) VALUES ('$nome','$tempo','$temporeal')";
 $resultado_recados = mysqli_query($conn,$result_recado);
 
-header("location: http://localhost:888/lazygame/");
+//header("location: http://localhost:888/lazygame/");
 
 
 ?>
